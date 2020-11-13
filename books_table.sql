7a885aaf788419ee1552e4661abbca2a6da5b603
Jaquelin Erazo <j.erazo1744@gmail.com>
2020-11-12 19:16:04 -0500

    initial commit

---
+++ books_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE books (
+    bookID integer,
+	title varchar,
+    authors varchar,
+    average_rating float,
+    isbn varchar,
+    isbn13 varchar,
+	language_code varchar,
+	num_pages integer,
+	ratings_count bigint,
+	text_reviews_count bigint,
+	publication_date date,
+	publisher varchar
+);
+
+-- deleted line 3350, 4704, 5879, 8981
